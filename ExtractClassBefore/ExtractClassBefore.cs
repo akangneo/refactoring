1ad679a0a91f56b8e423f8195b4cf357dc18a0a5
Arief Nugraha <akangneo@live.com>
2019-07-24 14:30:12 +0700

    update extract metode, interface, class dan superclass

---
+++ ExtractClassBefore/ExtractClassBefore.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net.Mail;
+
+namespace ExtractClass
+{
+    class Person
+    {
+        const string FilePath = @"c:\log.txt";
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public void SetAddress(string city, string address)
+        {
+            City = city;
+            Address = address;
+        }
+
+        public bool SendMailToAddress(string address, string subject, string text)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mail.From = new MailAddress(EmailAddress);
+                mail.To.Add(address);
+                mail.Subject = subject;
+                mail.Body = text;
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential(UserName, Password);
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                return true;
+            }
+            catch (Exception )
+            {
+                return false;
+            }
+        }
+
+        void Log(string message)
+        {
+            using (StreamWriter streamWriter = new StreamWriter(FilePath))
+            {
+                streamWriter.WriteLine(message);
+                streamWriter.Close();
+            }
+        }
+
+        public string Name { get; }
+        public int Age { get; }
+        public string City { get; private set; }
+        public string Address { get; private set; }
+        public string UserName { get; }
+        public string EmailAddress { get; }
+        public string Password { get; }
+    }
+}
---
+++ ExtractMethodBefore/ExtractMethodBefore.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExtractMethod
+{
+    class TextUtils
+    {
+        string[] FindEqualWords(string firstText, string secondText)
+        {
+            List<string> firstWordsList = new List<string>();
+            int textLength = firstText.Length;
+            int idx = 0;
+            string activeWord = string.Empty;
+            while (idx < textLength)
+            {
+                char ch = firstText[idx];
+                if (Char.IsLetter(ch))
+                    activeWord += ch;
+                else if (!string.IsNullOrEmpty(activeWord))
+                {
+                    firstWordsList.Add(activeWord);
+                    activeWord = string.Empty;
+                }
+            }
+            List<string> secondWordsList = new List<string>();
+            idx = 0;
+            activeWord = string.Empty;
+            textLength = secondText.Length;
+            while (idx < textLength)
+            {
+                char ch = secondText[idx];
+                if (Char.IsLetter(ch))
+                    activeWord += ch;
+                else if (!string.IsNullOrEmpty(activeWord))
+                {
+                    secondWordsList.Add(activeWord);
+                    activeWord = string.Empty;
+                }
+            }
+            List<string> result = new List<string>();
+            foreach (string word in firstWordsList)
+                if (secondWordsList.Contains(word))
+                    result.Add(word);
+            return result.ToArray();
+        }
+    }
+}
